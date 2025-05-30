29e94337a62449b009798dc6c9fdd3d78581f0f0
PhanNgocTuyen0401 <phangoctuyen040105@gmail.com>
2025-05-30 20:06:14 +0700

    first commit

---
+++ src/router/index.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/home/index";
+import Contact from "../pages/contact";
+import Layout from "../layout";
+export const router = createBrowserRouter ([
+    {
+        path: "/",
+        element: <Layout/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>
+            },
+            {
+                path: "/contact",
+                element: <Contact/>
+            },
+        ]
+    }
+]);
